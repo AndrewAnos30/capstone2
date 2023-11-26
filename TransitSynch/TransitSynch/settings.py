9a1944f17b05f18b9cd76233feffd3d8925991bd
Andrew Anos <96986357+AndrewAnos30@users.noreply.github.com>
2023-11-26 23:19:49 +0800

    balik

--- TransitSynch/TransitSynch/settings.py
+++ TransitSynch/TransitSynch/settings.py
@@ -9,9 +9,10 @@ https://docs.djangoproject.com/en/4.2/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.2/ref/settings/
 """
-
+import dj_database_url
 from pathlib import Path
 import os
+import django_heroku
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -24,9 +25,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-i4)yow^d4kf7@_ybumdp7yv9f497q5_!lt=ey0_uqqm6g3#_g='
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = []
 
 
 # Application definition
@@ -59,7 +60,6 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    'whitenoise.middleware.WhiteNoiseMiddleware',
 ]
 
 ROOT_URLCONF = 'TransitSynch.urls'
@@ -141,10 +141,6 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = '/static/'
-STATICFILES_DIRS =[BASE_DIR/"static"]
-STATIC_ROOT = BASE_DIR/ 'staticfiles'
-STATICFILES_STORAGE ="whitenoise.storage.CompressedManifestStaticFilesStorage"
-
 
 CRISPY_ALLOWED_TEMPLATE_PACKS = "bootstrap4"
 
@@ -181,3 +177,5 @@ RECAPTCHA_PROXY = {'http': 'http://127.0.0.1:8000', 'https': 'https://127.0.0.1:
 RECAPTCHA_DOMAIN = 'www.recaptcha.net'
 MEDIA_URL = '/media/'
 
+
+    
--- TransitSynch/staticfiles/main/custom.js
+++
@@ -1,9 +0,0 @@
-// Your custom JavaScript code can go here
-// This is just a placeholder
-
-// Example: Change background color on button click
-$(document).ready(function() {
-    $('#changeColorButton').click(function() {
-        $('body').css('background-color', 'lightblue'); // Change to your desired color
-    });
-});
